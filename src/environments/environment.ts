95f73342ea042d6adabb05b6f30e70a3af3752d6
Rubén Campal <ruben.campal.lopez1996@gmail.com>
2026-07-22 13:01:33 +0200

    update gitignore

--- src/environments/environment.ts
+++
@@ -1,8 +0,0 @@
-export const environment = {
-  currencyFreaksApiKey: '659faa218e684a37b26b6477d8e26090',
-  currencyFreaksApiUrl: 'https://api.currencyfreaks.com/v2.0/rates/latest',
-  currencyElTiempoNetNacional: 'https://api.el-tiempo.net/json/v3/general',
-  currencyElTiempoNetListaProvincias: 'https://api.el-tiempo.net/json/v3/provincias',
-  currencyElTiempoNetTiempoProvincia: 'https://api.el-tiempo.net/json/v3/provincias/[CODPROV]',
-};
-
